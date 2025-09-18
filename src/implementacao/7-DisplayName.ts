1f75ddeead1466d38497f82e6d43dcd1f6d09d30
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-18 15:20:03 -0300

    exercicio display name resolvido

--- src/implementacao/7-DisplayName.ts
+++ src/implementacao/7-DisplayName.ts
@@ -1,3 +1,9 @@
 export function DisplayName(primeiroNome: string, sobrenome: string, apelido: string): string {
-
+    if(apelido) {
+        return `${primeiroNome} "${apelido}" ${sobrenome}`;
+    }else if(sobrenome) {
+        return `${primeiroNome} ${sobrenome}`;
+    }else {
+        return primeiroNome;
+    }
 }
