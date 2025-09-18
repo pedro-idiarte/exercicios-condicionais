77a62e6c81f8e384969cf143ae4b730722a89c66
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-18 15:19:07 -0300

    exercicio roller coaster resolvido

--- src/implementacao/5-VeryScaryRollerCoaster.ts
+++ src/implementacao/5-VeryScaryRollerCoaster.ts
@@ -1,3 +1,9 @@
 export function verificaAcesso(idade: number, possuiPatologia: boolean, altura: number, ehEstudante: boolean): string {
-
+    if (idade < 12 ||idade > 65 || possuiPatologia || altura < 150) {
+        return "ACESSO NEGADO";
+    }else if (ehEstudante || idade < 18) {
+        return "10 reais";
+    }else {
+        return "20 reais";
+    }
 }
