4be8475760898c9b6ad16083e7e3fd65604d22e4
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-18 15:18:15 -0300

    exercicio naming stones resolvido

--- src/implementacao/3-NamingTheStones.ts
+++ src/implementacao/3-NamingTheStones.ts
@@ -1,3 +1,28 @@
 export function NamingTheStones(ladoA: number, ladoB: number): string {
 
+    if(ladoA === ladoB) {
+        switch(ladoA) {
+            case 0:
+                return "Bucha de Branco";
+            case 1:
+                return "Bucha de Ás";
+            case 2:
+                return "Bucha de Duque";
+            case 3:
+                return "Bucha de Terno";
+            case 4:
+                return "Bucha de Quadra";
+            case 5:
+                return "Bucha de Quina";
+            case 6:
+                return "Bucha de Sena";
+            default:
+                return "NÃO";
+        }
+    } else {
+        return "NÃO";
+    }
 }
+
+
+
