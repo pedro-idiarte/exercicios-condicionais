19ec3416a56cfdfa730da49aba97a9dcd2841329
Pedro Idiarte <pedroidiarte7@gmail.com>
2025-09-18 15:19:39 -0300

    exercicio discount resolvido

--- src/implementacao/6-BuyAtaDiscount.ts
+++ src/implementacao/6-BuyAtaDiscount.ts
@@ -1,3 +1,14 @@
 export function BuyAtaDiscount(valorOriginal: number, formaDePagamento: string): number {
+    let valorFinal: number;
 
-}
+    if (formaDePagamento === "dinheiro" || formaDePagamento === "pix") {
+        valorFinal = valorOriginal * 0.90; 
+    } else if (formaDePagamento === "debito") {
+        valorFinal = valorOriginal * 0.95; 
+    } else if (formaDePagamento === "credito") {
+        valorFinal = valorOriginal * 0.97; 
+    } else {
+        valorFinal = valorOriginal; 
+    }
+    return valorFinal / 100;
+} 
